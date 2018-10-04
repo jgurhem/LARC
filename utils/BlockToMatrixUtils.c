b991c8026369ad2e521d7f78ec261ec9be899461
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2018-10-04 07:04:01 +0200

    generation supports ordinary matrices

--- utils/BlockToMatrixUtils.c
+++ utils/BlockToMatrixUtils.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-double * genSingleMat(char * filePath, int bsize){
+double * genSingleMat(char * filePath, int nbRow, int nbCol){
 	FILE * f;
 	f = fopen(filePath, "r");
 	double * mat;
 
-	mat = (double*) malloc(bsize*bsize*sizeof(double));
+	mat = (double*) malloc(nbCol * nbRow * sizeof(double));
 
-	int i,j,indI,indJ;
+	int i, j, indI, indJ;
 	double tmp;
 
-	for(i = 0 ; i < bsize ; i++) { //row
-	  for(j = 0; j < bsize ; j++) { //col
-	  	fscanf(f,"%d %d %lf",&indI,&indJ,&tmp);
-		mat[indI*bsize + indJ] = tmp;
-	  	//printf("i-%d j-%d indi-%d indj-%d %lf \n",i,j,indI,indJ,tmp);
-	  }
-	  //printf("\n");
+	for(i = 0; i < nbRow; i++) { //row
+		for(j = 0; j < nbCol; j++) { //col
+			fscanf(f, "%d %d %lf", &indI, &indJ, &tmp);
+			mat[indI * nbCol + indJ] = tmp;
+			//printf("i-%d j-%d indi-%d indj-%d %lf \n",i,j,indI,indJ,tmp);
+		}
+		//printf("\n");
 	}
 	fclose(f);
 
@@ -46,14 +46,14 @@ void genMatrixBlock(char * filePath, int nb, int bsize, double * mat, int blockR
 	double tmp;
 
 	for(i = 0 ; i < bsize ; i++) { //row
-	  for(j = 0; j < bsize ; j++) { //col
-		//fscanf(f,"%lf",&tmp);
-	  	fscanf(f,"%d %d %lf",&indI,&indJ,&tmp);
-	  	//mat[(blockRow*bsize + i)*nb*bsize + blockCol*bsize + j] = tmp;
-		mat[(blockRow*bsize + indI)*nb*bsize + blockCol*bsize + indJ] = tmp;
-	  	//printf("i-%d j-%d indi-%d indj-%d %lf \n",i,j,indI,indJ,tmp);
-	  }
-	  //printf("\n");
+		for(j = 0; j < bsize ; j++) { //col
+			//fscanf(f,"%lf",&tmp);
+			fscanf(f,"%d %d %lf",&indI,&indJ,&tmp);
+			//mat[(blockRow*bsize + i)*nb*bsize + blockCol*bsize + j] = tmp;
+			mat[(blockRow*bsize + indI)*nb*bsize + blockCol*bsize + indJ] = tmp;
+			//printf("i-%d j-%d indi-%d indj-%d %lf \n",i,j,indI,indJ,tmp);
+		}
+		//printf("\n");
 	}
 	fclose(f);
 }
@@ -63,12 +63,12 @@ void genBlockUnit(int nb, int bsize, double * mat, int blockRow, int blockCol){
 	int i,j;
 
 	for(i = 0 ; i < bsize ; i++) { //row
-	  for(j = 0; j < bsize ; j++) { //col
-	  	if(j==i)
-	  		mat[(blockRow*bsize + i)*nb*bsize + blockCol*bsize + j] = 1;
-		else
-			mat[(blockRow*bsize + i)*nb*bsize + blockCol*bsize + j] = 0;
-	  }
+		for(j = 0; j < bsize ; j++) { //col
+			if(j==i)
+				mat[(blockRow*bsize + i)*nb*bsize + blockCol*bsize + j] = 1;
+			else
+				mat[(blockRow*bsize + i)*nb*bsize + blockCol*bsize + j] = 0;
+		}
 	}
 }
 
@@ -77,9 +77,20 @@ void genBlockZero(int nb, int bsize, double * mat, int blockRow, int blockCol){
 	int i,j;
 
 	for(i = 0 ; i < bsize ; i++) { //row
-	  for(j = 0; j < bsize ; j++) { //col
+		for(j = 0; j < bsize ; j++) { //col
 			mat[(blockRow*bsize + i)*nb*bsize + blockCol*bsize + j] = 0;
-	  }
+		}
+	}
+}
+
+void extractBlock(int nb, int bsize, double * src, double * dst, int blockRow, int blockCol){
+
+	int i,j;
+
+	for(i = 0 ; i < bsize ; i++) { //row
+		for(j = 0; j < bsize ; j++) { //col
+			dst[(blockRow*bsize + i)*nb*bsize + blockCol*bsize + j] = src[(blockRow*bsize + i)*nb*bsize + blockCol*bsize + j];
+		}
 	}
 }
 
@@ -95,6 +106,21 @@ double * genMatrixA(char* filePath, int nb, int bsize){
 	}
 
 	return mat;
+}
+
+double * extractMatrixU(int nb, int bsize, double *src){
+    double *mat;
+    int i, j;
+    mat = (double*) malloc(nb * nb * bsize * bsize * sizeof(double));
+    for(i = 0; i < nb; i++){
+        for(j = 0; j < nb; j++){
+            if(i <= j)
+                extractBlock(nb, bsize, src, mat, i, j);
+            else
+                genBlockZero(nb, bsize, mat, i, j);
+        }
+    }
+    return mat;
 }
 
 double * genMatrixU(char* filePath, int nb, int bsize){
@@ -114,6 +140,23 @@ double * genMatrixU(char* filePath, int nb, int bsize){
 	return mat;
 }
 
+double * extractMatrixL(int nb, int bsize, double *src){
+    double *mat;
+    int i, j;
+    mat = (double*) malloc(nb * nb * bsize * bsize * sizeof(double));
+    for(i = 0; i < nb; i++){
+        for(j = 0; j < nb; j++){
+            if(i > j)
+                extractBlock(nb, bsize, src, mat, i, j);
+            else if(i==j)
+                genBlockUnit(nb, bsize, mat,i ,j);
+            else
+                genBlockZero(nb, bsize, mat, i, j);
+        }
+    }
+    return mat;
+}
+
 double * genMatrixL(char* filePath, int nb, int bsize){
 
 	double * mat;
@@ -129,19 +172,18 @@ double * genMatrixL(char* filePath, int nb, int bsize){
 				genBlockZero(nb, bsize, mat,i ,j);
 		}
 	}
-
 	return mat;
 }
 
 void printMatrixLatex(double *mat, int nb, int bsize){
 	int i,j;
 	for(i = 0 ; i < bsize * nb ; i++) { //row
-	  for(j = 0; j < bsize * nb; j++) { //col
-		printf("%8.3lf",mat[ i*nb*bsize + j] );
-		if( j < bsize * nb - 1)
-			printf(" &");
-	  }
-	  printf("\\\\\n");
+		for(j = 0; j < bsize * nb; j++) { //col
+			printf("%8.3lf",mat[ i*nb*bsize + j] );
+			if( j < bsize * nb - 1)
+				printf(" &");
+		}
+		printf("\\\\\n");
 	}
 }
 
--- utils/BlockToMatrixUtils.h
+++ utils/BlockToMatrixUtils.h
@@ -2,11 +2,14 @@
 #define BLOCKTOMATRIXUTILS
 
 void genMatrixBlock(char * filePath, int nb, int bsize, double * mat, int blockRow, int blockCol);
-double * genSingleMat(char * filePath, int bsize);
+double * genSingleMat(char * filePath, int nbRow, int nbCol);
 
 double * genMatrixA(char* filePath, int nb, int bsize);
 double * genMatrixL(char* filePath, int nb, int bsize);
 double * genMatrixU(char* filePath, int nb, int bsize);
+
+double * extractMatrixL(int nb, int bsize, double *src);
+double * extractMatrixU(int nb, int bsize, double *src);
 
 void printMatrixLatex(double *mat, int nb, int bsize);
 void cleanMatrix(double *mat);
