d78f28fa7966ba70b39af344ad2dcf75e1242d75
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2019-04-24 18:00:01 +0000

    rewrite some tests

--- tests/dgeaxpxmv.c
+++ tests/dgeaxpxmv.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,27 +7,43 @@
 
 int main(int argc, char **argv) {
 
-  double *m, *v, *y;
+  double *m, *v, *y, *r;
   int size = 5;
-  v = malloc(size * sizeof(double));
+  v = calloc(size, sizeof(double));
+  r = calloc(size, sizeof(double));
   // m = genMat(size, size);
   m = calloc(size * size, sizeof(double));
-  ;
 
   for (int i = 0; i < size; i++) {
-    for (int k = 0; k < size; k++) {
-      m[i + size * k] = i + size * k;
-    }
+    m[i + size * i] = 1;
     v[i] = i + 1;
+    r[i] = 3 * (i + 1);
   }
 
   printf("a\n");
   printMatrix(m, size, size);
-  printf("\n\nv\n");
+  printf("v\n");
   printVect(v, size);
 
   y = dgeaxpxmv(m, v, size);
 
-  printf("\n\nv\n");
+  printf("r\n");
   printVect(y, size);
+  printf("r - computed\n");
+  printVect(r, size);
+
+  double norm = diffNorm(y, r, size);
+  printf("norm = %lf\n", norm);
+  if (isnan(norm)) {
+    printf("error : norm = nan\n");
+    exit(1);
+  }
+  if (norm - 0.0001 > 0) {
+    printf("error : norm > 0.0001\n");
+    exit(1);
+  }
+  free(y);
+  free(v);
+  free(r);
+  free(m);
 }
--- tests/powerIt.c
+++ tests/powerIt.c
@@ -1,18 +1,37 @@
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <mvoputils.h>
 
 int main(int argc, char **argv) {
 
   double *m, *v, l;
-  int size = 5;
-  m = genMat(size, size);
+  int size = 4;
+  // m = genMat(size, size);
+  m = calloc(size * size, sizeof(double));
   v = genVect(size);
+
+  m[0 * size + 0] = 2;
+  m[1 * size + 1] = 2;
+  m[2 * size + 2] = 3;
+  m[3 * size + 3] = 3;
+  m[3 * size + 2] = 1;
+  m[2 * size + 1] = 1;
+  m[1 * size + 0] = 1;
 
   printf("a\n");
   printMatrix(m, size, size);
   printf("vect init\n");
   printVect(v, size);
-  l = powerIt(m, v, size, 0.00000001, 10000);
+  l = powerIt(m, v, size, 0.0000000001, 100000);
   printf("l1 = %lf\n", l);
+  if (isnan(l)) {
+    printf("error : l = nan\n");
+    exit(1);
+  }
+  if (abs(l - 3) > 0.0001) {
+    printf("error : abs(expected - real) > 0.0001\n");
+    exit(1);
+  }
 }
